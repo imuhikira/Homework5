dba95ba183d89080c1836c8ebfe4465720e4d00e
Muhikira <imuhikira@gmail.com>
2019-02-20 23:30:47 -0600

    Homework5

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+package edu.dmacc.codedsm.hw5;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        double a= Math.sqrt(27);
+        System.out.println(a);
+
+        int x= 28/5;
+        System.out.println(x);
+
+        double y = Math.pow(2.2,4);
+        System.out.println(y);
+
+        double z = Math.random()*10;
+        System.out.println((int)z);
+
+    }
+}
